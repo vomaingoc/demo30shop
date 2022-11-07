f777871ef367e78bf538b8379b7c6b3f5ea2ac11
vomaingoc <vomaingoc@gmail.com>
2022-11-07 22:58:34 +0700

    react root

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-// import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
 import "styles.css";
 import "antd/dist/antd.css";
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById("root") as Element;
 const root = ReactDOMClient.createRoot(rootElement);
 
 root.render(
-  // <StrictMode>
-  <App />
-  // </StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
