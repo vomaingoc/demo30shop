9217afccfd08d4b2e6533460c77f1bca6625b3c3
vomaingoc <vomaingoc@gmail.com>
2022-11-08 08:45:37 +0700

    react root2

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,9 @@ import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
 import "styles.css";
 import "antd/dist/antd.css";
-const rootElement = document.getElementById("root") as Element;
-const root = ReactDOMClient.createRoot(rootElement);
+
+const rootElement = document.getElementById("root");
+const root = ReactDOMClient.createRoot(rootElement as HTMLElement);
 
 root.render(
   <StrictMode>
